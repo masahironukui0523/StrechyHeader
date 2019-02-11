243599ae10f948bee46e01e47b8fa2e10e71e5ce
masa <mickey24352002@gmail.com>
2019-02-11 13:50:16 +0900

    Initial Commit

---
+++ StrechyHeader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StrechyHeader
+//
+//  Created by 抜井正寛 on 2019/02/11.
+//  Copyright © 2019 Masahiro Nukui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
